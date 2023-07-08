d9cb21de2588b2b334022d263c069a7dc036e595
bjorgkav <yovivstolkan@gmail.com>
2023-07-08 17:28:07 +0800

    edit client app code, updated required client files

--- client/GUI/dashingShell512.sh
+++ client/GUI/dashingShell512.sh
@@ -3,11 +3,15 @@
 #given the data from the paths indicated in path.txt,
 #split the each sequence into k-mers, and 
 #compute the HLL sketch of each sequence, with a sketch size (spacing) of 9
-#./dashing_s512 sketch -k31 -p13 -S9 -F path.txt
 
 echo "Running Dashing tool..."
 
 ./dashing_s512 sketch -k31 -p13 -S9 fastas/*.fasta
+
+if [ $? -ne 0 ]; then
+    echo "Error encountered using this dashing binary. Use a different binary."
+    exit 1
+fi
 
 echo "Reading output and Creating CSV..."
 
--- client/GUI/readHLLandWrite256.sh
+++ client/GUI/readHLLandWrite256.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#clear output file (repeated testing)
+#clear output file
 rm ./output.txt
 
-#assume no is 512 (based on Sim et al.'s paper)
+#assume no is 512
 echo -n "Accession ID," >> ./output.txt
 
 no_of_features=512
@@ -16,11 +16,6 @@ printf "%s\n" "feature_$i" >> ./output.txt
 for f in ./fastas/*.hll
 do 
     content=$(./dashing_s256 view $f)
-
-#    $f = ${f#"clientDashing"}
-#    $f = ${f%".fasta.w.31.spacing.9.hll"}
-
-    #sed 's/[//g;]//g'
     echo "$f, $content" >> ./output.txt
 done
 
@@ -29,7 +24,6 @@ echo "Removing [ and ] from output and placing in csv..."
 echo -n "$(sed -i 's/[][]//g' ./output.txt)"
 echo -n "$(sed -i 's/\<fastas\>//g' ./output.txt)"
 echo -n "$(sed -i 's/\///g' ./output.txt)"
-#| sed -e 's/\<FASTAFiles\>//g' | sed -e 's/\///g'
 echo -n "$(sed -i 's/\.//g' ./output.txt)"
 echo "$(sed -i 's/fastas//g' ./output.txt)"
 echo "$(sed -i 's/fastaw31spacing9hll//g' ./output.txt)"
